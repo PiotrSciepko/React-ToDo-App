8163e6649a516270477377b6068e2d852f7fca27
PiotrSciepko <piotrek.priv@gmail.com>
2021-05-04 12:19:47 +0200

    add getTasks() & constant keys

---
+++ js/api/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "6594e43e-01cd-4eb9-a27e-240b4fe4808b";
+export const API_URL = "https://todo-api.coderslab.pl/api";
--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
+import {getTasks} from "./api/tasks";
 
 function App() {
-  return <h1>Hello World</h1>
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        getTasks(setTasks)
+    }, [])
+
+    return (
+        <>
+            {tasks.map(task=>task.toString())}
+        </>
+    )
 }
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
